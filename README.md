643e9f465f991e599b9ce6f48fb97ec6ecb6d37a
YogyaGamerz13 <77455577+YogyaGamerz13@users.noreply.github.com>
2021-07-21 20:29:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Marathon-Game-3
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,57 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var backgroundImage
+
+var form, player, game;
+
+var runner1, runner2, runner3, runner4, runners;
+
+var runner1Image, runner2Image, runner3Image, runner4Image, groundImage, trackImage;
+
+var finishedPlayers = 0;
+
+function preload(){
+  backgroundImage = loadImage('images/Marathon.jpg')
+  runner1Image = loadGif("images/Runner1.gif")
+  runner2Image = loadGif("images/Runner2.gif")
+  runner3Image = loadGif("images/Runner3.gif")
+  runner4Image = loadGif("images/Runner4.gif")
+  groundImage = loadImage("images/ground.png")
+  trackImage = loadImage("images/tracktemp.png")
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth -20, displayHeight -200);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  //start the game
+  if(finishedPlayers === 0 && playerCount === 4){
+    game.update(1)
+  }
+
+  if(finishedPlayers === 4){
+    game.update(2)
+  }
+
+  if(finishedPlayers === 4 && gameState === 2){
+    game.displayRank()
+  }
+   
+}
